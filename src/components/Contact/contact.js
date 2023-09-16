256f3ddc43b2b9ae6f8105c0bc0c347d2528a9c0
Sarah Squyres <ssquyres15@gmail.com>
2023-09-16 14:43:15 -0500

    link functionality, contact email functionality, responsiveness

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './contact.css';
 import FacebookIcon from "../../assets/facebook-icon.png"
 import TwitterIcon from "../../assets/twitter.png"
 import InstagramIcon from "../../assets/instagram.png"
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_jvjapvn', 'template_qfyyu8g', form.current, '0_MRd4Y2uSEqIyCSK')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
     return (
         <section id="contactPage">
             <div id="contact">
@@ -12,15 +24,15 @@ const Contact = () => {
                 <span className="contactDesc">
                     Please fill out the form if you have any questions!
                 </span>
-                <form className='contactForm'>
-                    <input type='text' className='name' placeholder='Your Name'/>
-                    <input type='email' className='email' placeholder='Email'/>
+                <form className='contactForm' ref={form} onSubmit={sendEmail}>
+                    <input type='text' className='name' placeholder='Your Name' name="your_name" />
+                    <input type='email' className='email' placeholder='Email' name="your_email"/>
                     <textarea className='msg' name="message" rows="5" placeholder='Your message here'></textarea>
                     <button type="submit" value="Send" className="submitBtn">Submit</button>
                     <div className='links'>
-                        <img src={FacebookIcon} alt="Facebook" className='link'/>
-                        <img src={TwitterIcon} alt="Twitter" className='link'/>
-                        <img src={InstagramIcon}  alt="Instagram" className='link'/>
+                        <img src={FacebookIcon} alt="Facebook" className='link' />
+                        <img src={TwitterIcon} alt="Twitter" className='link' />
+                        <img src={InstagramIcon} alt="Instagram" className='link' />
 
                     </div>
                 </form>
