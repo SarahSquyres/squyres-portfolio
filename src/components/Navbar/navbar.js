ab2ed37833c9c86c6d45fa312781503ee19c2687
Sarah Squyres <ssquyres15@gmail.com>
2023-09-15 22:27:42 -0500

    navbar spacing

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './navbar.css';
 import logo from '../../assets/logo.png';
-
+import { Link } from 'react-scroll';
 
 const Navbar = () => {
     return (
         <nav className="navbar">
             <img src={logo} alt="Logo" className="logo" />
             <div className="desktopMenu">
-
+                <Link className="desktopMenuListItem">Home</Link>
+                <Link className="desktopMenuListItem">About Me</Link>
+                <Link className="desktopMenuListItem">Portfolio</Link>
+                <Link className="desktopMenuListItem">Contact</Link>
+                <Link className="desktopMenuListItem">Resume</Link>
             </div>
             <button className="desktopMenuBtn">
                 <img src="" alt="" className="desktopMenuImg" />Contact Me
